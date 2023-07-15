23fe3d6c05963139756499d7d3bd241fe12dba48
윤동현 <donghyunyoon81@gmail.com>
2023-07-15 14:46:55 +0900

    record<Statement> : 1.6 계산 단계와 포맷팅 단계 분리하기
    내부함수로 구현 했을경우에 결과 형식을 변경하고 싶을때
    statementData를 생성해서 거기에 필요한 값들을 선언해서
    형식에 맞게 출력하는 함수에 데이터를 주는 방식이다.
    javaScript로 작성되어 있어서 kotlin으로 변환하기 어려워 다음에 구현해보려고 한다.

--- src/main/kotlin/chapter_1/Statement.kt
+++ src/main/kotlin/chapter_1/Statement.kt
@@ -6,60 +6,62 @@ data class Play(val name: String, val type: String)
 data class Invoice(val customer: String, val performances: List<Performance>)
 data class Performance(val playID: String, val audience: Int)
 
-fun main(){
+fun main() {
     val plays = listOf(
-        Play("Hamlet","tragedy"),
-        Play("As you Like it","comedy"),
-        Play("Othello","tragedy")
+        Play("Hamlet", "tragedy"),
+        Play("As you Like it", "comedy"),
+        Play("Othello", "tragedy")
     )
-    val invoices = Invoice("BigCo",
-            listOf(
-                Performance("Hamlet",55),
-                Performance("As you Like it",35),
-                Performance("Othello",40)
-            )
+    val invoices = Invoice(
+        "BigCo",
+        listOf(
+            Performance("Hamlet", 55),
+            Performance("As you Like it", 35),
+            Performance("Othello", 40)
         )
-    println(Statement(invoices,plays).statement())
+    )
+    println(Statement(invoices, plays).statement())
 }
-class Statement(private val invoice: Invoice, private val plays: List<Play>){
-    fun statement():String {
+
+class Statement(private val invoice: Invoice, private val plays: List<Play>) {
+    fun statement(): String {
+        return renderPlainText()
+    }
+    fun renderPlainText(): String {
         var result = "청구 내약 (고객명: ${invoice.customer})\n"
-        for (performance in invoice.performances){
-            result += "${playFor(performance).name}: $${amountFor(performance)/100} (${performance.audience}석)\n"
+        for (performance in invoice.performances) {
+            result += "${playFor(performance).name}: $${amountFor(performance) / 100} (${performance.audience}석)\n"
         }
-        result += "총액: ${totalAmount()/100}\n"
+        result += "총액: ${totalAmount() / 100}\n"
         result += "적립포인트: ${totalVolumeCredits()}점\n"
         return result
     }
-    fun totalAmount() : Int{ // statement의 totalAmount 임시변수를 대체
-        var result = 0
-        for (performance in invoice.performances){
-            result += amountFor(performance)
-        }
-        return result
-    }
-    fun totalVolumeCredits():Double{ // statement의 volumeCredits 임시변수를 대체
-        var volumeCredits = 0.0
-        for (performance in invoice.performances){
-            volumeCredits +=volumeCreditsFor(performance)
-        }
-        return volumeCredits
-    }
-    fun volumeCreditsFor(perf:Performance) =
-        if (playFor(perf).type == "comedy") floor(perf.audience.toDouble()/5 )+maxOf(perf.audience -30,0).toDouble()
-        else maxOf(perf.audience -30,0).toDouble()
+
+    // statement의 totalAmount 임시변수를 대체
+    fun totalAmount(): Int = invoice.performances.sumOf { amountFor(it) }
+
+    // statement의 volumeCredits 임시변수를 대체
+    fun totalVolumeCredits(): Double = invoice.performances.sumOf { volumeCreditsFor(it) }
+
+    fun volumeCreditsFor(perf: Performance) =
+        if (playFor(perf).type == "comedy") floor(perf.audience.toDouble() / 5) + maxOf(
+            perf.audience - 30,
+            0
+        ).toDouble()
+        else maxOf(perf.audience - 30, 0).toDouble()
 
     fun playFor(perf: Performance) = plays.find { it.name == perf.playID }!!
-    fun amountFor(perf: Performance) : Int{
+    fun amountFor(perf: Performance): Int {
         var result = 0 // 함수의 결과를 반환한다는 의미로 변수이름을 result로 지어주는게 좋다.
-        when(playFor(perf).type){ // 희극, 코메디만 적용한 함수지만 후에 더 많은 조건이 생길 경우 이 함수의 복잡도가 너무 크게 증가한다
+        when (playFor(perf).type) { // 희극, 코메디만 적용한 함수지만 후에 더 많은 조건이 생길 경우 이 함수의 복잡도가 너무 크게 증가한다
             "tragedy" -> {
                 result = 40000
-                if (perf.audience > 30) result += 1000 *(perf.audience -30)
+                if (perf.audience > 30) result += 1000 * (perf.audience - 30)
             }
+
             "comedy" -> {
                 result = 30000
-                if (perf.audience > 20) result += 10000 + 500 *(perf.audience -20)
+                if (perf.audience > 20) result += 10000 + 500 * (perf.audience - 20)
                 result += 300 * perf.audience
             }
         }
