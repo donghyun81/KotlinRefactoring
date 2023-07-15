17a105a81a7f9cea03182ca2e8d34f65f593971d
윤동현 <donghyunyoon81@gmail.com>
2023-07-15 14:04:35 +0900

    record<Statement> : 1.4 statement() 함수 쪼개기
    1. 조건에 따라 계산하는 부분을 먼저 다른 내부 함수로 출력한다 (내부 함수로 출력하는 이유는 매개변수를 줄일 수 있기 때문에)
    2. 임시 변수를 제거하기 위해서 임시 변수를 할당할때에 값을 함수로 추출해서 반환하도록 한다.(반복문일 경우에 시간에 지장을 줄 정도로 크지 않을 경우 나눈다)

--- src/main/kotlin/chapter_1/Statement.kt
+++ src/main/kotlin/chapter_1/Statement.kt
@@ -19,34 +19,52 @@ fun main(){
                 Performance("Othello",40)
             )
         )
-    println(statement(invoices,plays))
+    println(Statement(invoices,plays).statement())
 }
-fun statement(invoice: Invoice, plays: List<Play>):String {
-    var totalAmount = 0
-    var volumeCredits = 0.0
-    var result = "청구 내약 (고객명: ${invoice.customer})\n"
-    for (performance in invoice.performances){
-        val play = plays.find { it.name == performance.playID }!!
-        var thisAmount = 0
-        when(play.type){ // 희극, 코메디만 적용한 함수지만 후에 더 많은 조건이 생길 경우 이 함수의 복잡도가 너무 크게 증가한다
+class Statement(private val invoice: Invoice, private val plays: List<Play>){
+    fun statement():String {
+        var result = "청구 내약 (고객명: ${invoice.customer})\n"
+        for (performance in invoice.performances){
+            result += "${playFor(performance).name}: $${amountFor(performance)/100} (${performance.audience}석)\n"
+        }
+        result += "총액: ${totalAmount()/100}\n"
+        result += "적립포인트: ${totalVolumeCredits()}점\n"
+        return result
+    }
+    fun totalAmount() : Int{ // statement의 totalAmount 임시변수를 대체
+        var result = 0
+        for (performance in invoice.performances){
+            result += amountFor(performance)
+        }
+        return result
+    }
+    fun totalVolumeCredits():Double{ // statement의 volumeCredits 임시변수를 대체
+        var volumeCredits = 0.0
+        for (performance in invoice.performances){
+            volumeCredits +=volumeCreditsFor(performance)
+        }
+        return volumeCredits
+    }
+    fun volumeCreditsFor(perf:Performance) =
+        if (playFor(perf).type == "comedy") floor(perf.audience.toDouble()/5 )+maxOf(perf.audience -30,0).toDouble()
+        else maxOf(perf.audience -30,0).toDouble()
+
+    fun playFor(perf: Performance) = plays.find { it.name == perf.playID }!!
+    fun amountFor(perf: Performance) : Int{
+        var result = 0 // 함수의 결과를 반환한다는 의미로 변수이름을 result로 지어주는게 좋다.
+        when(playFor(perf).type){ // 희극, 코메디만 적용한 함수지만 후에 더 많은 조건이 생길 경우 이 함수의 복잡도가 너무 크게 증가한다
             "tragedy" -> {
-                thisAmount = 40000
-                if (performance.audience > 30) thisAmount += 1000 *(performance.audience -30)
+                result = 40000
+                if (perf.audience > 30) result += 1000 *(perf.audience -30)
             }
             "comedy" -> {
-                thisAmount = 30000
-                if (performance.audience > 20) thisAmount += 10000 + 500 *(performance.audience -20)
-                thisAmount += 300 * performance.audience
+                result = 30000
+                if (perf.audience > 20) result += 10000 + 500 *(perf.audience -20)
+                result += 300 * perf.audience
             }
-            else -> {}
         }
-        volumeCredits += maxOf(performance.audience -30,0)
-        if (play.type == "comedy") volumeCredits += floor(performance.audience.toDouble()/5 )
-        // Html로 출력하는 기능이 필요해서 이 청구 결과에 문자열을 추가하는 문장 각각 조건문으로 감싸야함
-        result += "${play.name}: $${thisAmount/100} (${performance.audience}석)\n"
-        totalAmount += thisAmount
+        return result
     }
-        result += "총액: ${totalAmount/100}\n"
-        result += "적립포인트: ${volumeCredits}점\n"
-    return result
+
+
 }
--- src/test/kotlin/StatementTest.kt
+++ src/test/kotlin/StatementTest.kt
@@ -1,7 +1,4 @@
-import chapter_1.Invoice
-import chapter_1.Performance
-import chapter_1.Play
-import chapter_1.statement
+import chapter_1.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -20,13 +17,13 @@ class StatementTest {
     )
 
     @Test
-    fun `statement 테스트`(){
+    fun `statement 테스트`(){ // 리팩터링이 잘 되었는지 확인할때마다 사용(간단하게 만들었는데도 매우 유용함)
         val expected = "청구 내약 (고객명: BigCo)\n" +
                 "Hamlet: \$650 (55석)\n" +
                 "As you Like it: \$580 (35석)\n" +
                 "Othello: \$500 (40석)\n" +
                 "총액: 1730\n" +
                 "적립포인트: 47.0점\n"
-        assertThat(expected).isEqualTo(statement(invoices,plays))
+        assertThat(expected).isEqualTo(Statement(invoices,plays).statement())
     }
 }
